d4496472fd9ade84dc27597964edf26f32674139
amira <amiraabouhadid@gmail.com>
2021-07-05 21:17:24 +0200

    Update local storage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -2,13 +2,13 @@ import bookList from './book-list.js';
 
 class LocalStorage {
   saveBookData = (bookList) => {
-    localStorage.clear();
-    localStorage.setItem('bookList', JSON.stringify(bookList));
+    window.localStorage.clear();
+    window.localStorage.setItem('bookList', JSON.stringify(bookList));
   };
 
   loadInputData = () => {
     if (localStorage.getItem('bookList') !== null) {
-      const loadedData = JSON.parse(localStorage.getItem('bookList'));
+      const loadedData = JSON.parse(window.localStorage.getItem('bookList'));
 
       loadedData.forEach((book) => {
         bookList.addBookToList(book);
