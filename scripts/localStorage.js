cf25b63a92957e8e632e64c05d3acc07e0e85f02
Gabriel <gabriel.gabrielyea@gmail.com>
2021-07-06 12:17:11 -0500

    Add documentation to localStorage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,15 +1,19 @@
 import bookList from './book-list.js';
 
+// Class that manages load and save events
 class LocalStorage {
+  // Saves book data, Its called after create or remove book from list.
   saveBookData = (bookList) => {
     window.localStorage.clear();
     window.localStorage.setItem('bookList', JSON.stringify(bookList));
   };
 
+  // Loads the input data, its called on load.
   loadInputData = () => {
     if (localStorage.getItem('bookList') !== null) {
       const loadedData = JSON.parse(window.localStorage.getItem('bookList'));
 
+      // Adds books found in local storage to the book list.
       loadedData.forEach((book) => {
         bookList.addBookToList(book);
       });
@@ -17,5 +21,6 @@ class LocalStorage {
   };
 }
 
+// Class instance.
 const storage = new LocalStorage();
 export { storage as default };
