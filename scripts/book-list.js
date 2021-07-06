09a7a98d1de434ae38ba638c767d1688e32190ce
Gabriel <gabriel.gabrielyea@gmail.com>
2021-07-06 11:54:48 -0500

    Add documentation to book-list

--- scripts/book-list.js
+++ scripts/book-list.js
@@ -1,23 +1,45 @@
 class BookList {
   bookList = [];
 
+  /**
+ * Adds a book to the book list.
+ *
+ * @param {Book} book an object type of Book
+ */
   addBookToList = (book) => {
     this.bookList = this.bookList.concat(book);
   }
 
+  /**
+ * Removes book by id
+ *
+ * @param {number} id The id of the selected book
+ */
   removeBookFromList = (id) => {
     this.bookList = this.bookList.filter((book) => book.id !== id);
   }
 
+  /**
+ * Returns the bookList
+ *
+ * @return {Array} booklist.
+ */
   get getList() {
     return this.bookList;
   }
 
+  /**
+ * Returns book by id
+ *
+ * @param {number} id The id of the selected book
+ * @return {Book} foundBook.
+ */
   getBookById = (id) => {
     const foundBook = this.bookList.find((book) => book.id === id);
     return foundBook;
   }
 }
 
+// Class instance
 const bookList = new BookList();
 export { bookList as default };
