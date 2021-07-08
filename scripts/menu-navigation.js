24e17864b1325d0cef910f99ce8c6784e6df1d3e
Gabriel <gabriel.gabrielyea@gmail.com>
2021-07-08 12:37:00 -0500

    Add second draft of menu-navigation: Dynamically assign links and targets

--- scripts/menu-navigation.js
+++ scripts/menu-navigation.js
@@ -1,16 +1,22 @@
 import listeners from './listeners.js';
 
 export default class MenuNavigation {
-  toAllBooks = document.querySelector('#test');
+  toAllBooks = document.querySelector('#nav-1');
 
-  allBooks = document.querySelector('.books-container');
+  navigationLinks = document.querySelector('.nav-links-container').getElementsByTagName('li');
+
+  navigationTargets = [document.querySelector('.books-container'),
+    document.querySelector('.input-container'),
+    document.querySelector('.contact-information'),
+  ]
 
   linksDic = {}
 
   init = () => {
     this.populateDict();
-    listeners.onClickEvent(this.toAllBooks,
-      { callback: this.displayMessage, param: this.linksDic[this.toAllBooks.id] });
+    this.assignLinkToTarget();
+    // listeners.onClickEvent(this.toAllBooks,
+    //   { callback: this.displayMessage, param: this.linksDic[this.toAllBooks.id] });
   }
 
   displayMessage = (obj) => {
@@ -18,6 +24,18 @@ export default class MenuNavigation {
   }
 
   populateDict = () => {
-    this.linksDic[this.toAllBooks.id] = this.allBooks;
+    Object.entries(this.navigationLinks).forEach((element) => {
+      this.linksDic[element[1].id] = {
+        link: element[1],
+        target: this.navigationTargets[element[0]],
+      };
+    });
+  }
+
+  assignLinkToTarget = () => {
+    Object.entries(this.linksDic).forEach((dictionaryEntry) => {
+      listeners.onClickEvent(dictionaryEntry[1].link,
+        { callback: this.displayMessage, param: dictionaryEntry[1].target });
+    });
   }
 }
