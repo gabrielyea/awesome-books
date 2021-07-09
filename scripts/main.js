fabb2075dbbda58aa18293987cea680c2650d336
Gabriel <gabriel.gabrielyea@gmail.com>
2021-07-09 10:52:49 -0500

    Add styling to nav bar and footer

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import BookUtilities from './book-utilities.js';
 import storage from './localStorage.js';
 import listeners from './listeners.js';
 import MenuNavigation from './menu-navigation.js';
+import DateTime from './libraries/luxon.js';
 
 const form = document.querySelector('form');
 
@@ -11,6 +12,12 @@ const navigation = new MenuNavigation();
 
 navigation.init();
 
+const dateTime = () => {
+  const dt = DateTime.now();
+  const dateTime = document.querySelector('.time-display');
+  dateTime.textContent = dt.toLocaleString(DateTime.DATETIME_MED);
+};
+
 listeners.onSubmitEvent(form,
   { callback: bookUtilities.saveBook },
   { callback: bookUtilities.displayAllBooks });
@@ -18,4 +25,5 @@ listeners.onSubmitEvent(form,
 window.addEventListener('load', () => {
   storage.loadInputData();
   bookUtilities.displayAllBooks();
+  dateTime();
 });
