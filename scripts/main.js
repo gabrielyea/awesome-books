92fdfc48f2148c7b5a6217f675812c515a74036b
Gabriel <gabriel.gabrielyea@gmail.com>
2021-07-09 11:07:03 -0500

    Fix linter

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import BookUtilities from './book-utilities.js';
 import storage from './localStorage.js';
 import listeners from './listeners.js';
 import MenuNavigation from './menu-navigation.js';
-import DateTime from './libraries/luxon.js';
+import luxon from './libraries/luxon.js';
 
 const form = document.querySelector('form');
 
@@ -10,14 +10,14 @@ const bookUtilities = new BookUtilities();
 
 const navigation = new MenuNavigation();
 
-const dateTime = DateTime;
+const dateTime = luxon;
 
 navigation.init();
 
 const setTime = () => {
   const dt = dateTime.now();
   const timeContainer = document.querySelector('.time-display');
-  timeContainer.textContent = dt.toLocaleString(DateTime.DATETIME_MED);
+  timeContainer.textContent = dt.toLocaleString(dateTime.DATETIME_MED);
 };
 
 listeners.onSubmitEvent(form,
